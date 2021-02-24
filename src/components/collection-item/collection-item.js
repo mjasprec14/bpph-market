66e95b4f9b152d35a3d4eb212a344580764694ba
mjaspec14 <michael@nextfinancial.io>
2021-02-24 23:40:31 +0800

    updated cart utils to group item in cart reducer

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -2,11 +2,14 @@ import React from "react";
 import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button";
 import { addFavorite } from "../../redux/favorites/favoritesActions";
+import { addItem } from "../../redux/cart/cartActions";
+import { FiShoppingCart } from "react-icons/fi";
+import { FiHeart } from "react-icons/fi";
 import "./collection-item.scss";
 import _ from "lodash";
 import numeral from "numeral";
 
-const CollectionItem = ({ item, addFavorite }) => {
+const CollectionItem = ({ item, addFavorite, addItem }) => {
   const {
     id,
     name,
@@ -26,7 +29,14 @@ const CollectionItem = ({ item, addFavorite }) => {
       <div
         className="collection-item__image"
         style={{ backgroundImage: `url(${imageUrl})` }}
-      ></div>
+      >
+        <span
+          className="collection-item__favorite-icon-container"
+          onClick={() => addFavorite(item)}
+        >
+          <FiHeart className="collection-item__favorite-icon" />
+        </span>
+      </div>
       <div className="collection-item__footer">
         <span className="collection-item__name">{_.capitalize(name)}</span>
         <span className="collection-item__price">{`₱${numeral(price).format(
@@ -34,8 +44,14 @@ const CollectionItem = ({ item, addFavorite }) => {
         )}`}</span>
       </div>
 
-      <CustomButton onClick={() => addFavorite(item)}>
+      {/* <CustomButton >
         Add to favorites
+      </CustomButton> */}
+      <CustomButton inverted onClick={() => addItem(item)}>
+        Add to cart{" "}
+        <span className="cart-icon__container">
+          <FiShoppingCart className="collection-item__cart-icon" />
+        </span>
       </CustomButton>
     </div>
   );
@@ -43,6 +59,7 @@ const CollectionItem = ({ item, addFavorite }) => {
 
 const mapDispatchToProps = dispatch => ({
   addFavorite: item => dispatch(addFavorite(item)),
+  addItem: item => dispatch(addItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/favorites/favorites.js
+++ src/components/favorites/favorites.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import { toggleFavoritesHidden } from "../../redux/favorites/favoritesActions";
-import { FiStar } from "react-icons/fi";
+import { FiHeart } from "react-icons/fi";
 import "./favorites.scss";
 
 const Favorites = ({ toggleFavoritesHidden }) => (
   <div className="favorites" onClick={toggleFavoritesHidden}>
-    <FiStar className="favorites__icon" />
+    <FiHeart className="favorites__icon" />
     <span className="favorites__item-count">0</span>
   </div>
 );
